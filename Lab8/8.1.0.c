385e9a1d252629380705508be223427ca6e0c921
RushilJalal <rushiljalal@gmail.com>
2023-10-15 12:07:21 +0530

    Add revlist(), improve code formatting

--- Lab8/8.1.0.c
+++ Lab8/8.1.0.c
@@ -1,80 +1,93 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct node{
+typedef struct node
+{
     struct node *prev;
     int data;
     struct node *next;
-}node;
+} node;
 
 node *head = NULL;
-//TRAVERSING THE LINKED LIST (PART7)
-void display(){
+// TRAVERSING THE LINKED LIST (PART7)
+void display()
+{
     node *ptr;
-    for(ptr = head;ptr != NULL;ptr = ptr->next){
-        printf("%d\n",ptr->data);
+    for (ptr = head; ptr != NULL; ptr = ptr->next)
+    {
+        printf("%d\n", ptr->data);
     }
 }
 
-void InsertAtBeginning(int val){
-    node *ptr = (node*)malloc(sizeof(node));
+void InsertAtBeginning(int val)
+{
+    node *ptr = (node *)malloc(sizeof(node));
     ptr->data = val;
-    if (head == NULL){
+    if (head == NULL)
+    {
         head = ptr;
         ptr->next = NULL;
         ptr->prev = NULL;
     }
-    else {
+    else
+    {
         ptr->next = head;
         ptr->prev = NULL;
         head->prev = ptr;
         head = ptr;
     }
 }
-//INSERTING AT THE REAR END (PART1)
-void insertAtEnd(int val){
-    node *ptr = (node*)malloc(sizeof(node));
+// INSERTING AT THE REAR END (PART1)
+void insertAtEnd(int val)
+{
+    node *ptr = (node *)malloc(sizeof(node));
     ptr->data = val;
-    if(head == NULL){
+    if (head == NULL)
+    {
         head = ptr;
         ptr->next = NULL;
         ptr->prev = NULL;
         return;
     }
     node *temp = head;
-    while(temp->next != NULL){
+    while (temp->next != NULL)
+    {
         temp = temp->next;
     }
-        ptr->next = NULL;
-        temp->next = ptr;
-        ptr->prev = temp;
+    ptr->next = NULL;
+    temp->next = ptr;
+    ptr->prev = temp;
 }
-//INSERT AT SPECIFIC POSITION (PART3)
-void InsertAtPos(int pos,int val){
-    node* ptr = (node*)malloc(sizeof(node));
+// INSERT AT SPECIFIC POSITION (PART3)
+void InsertAtPos(int pos, int val)
+{
+    node *ptr = (node *)malloc(sizeof(node));
     ptr->data = val;
-    if(head == NULL){
+    if (head == NULL)
+    {
         head = ptr;
         ptr->next = NULL;
         ptr->prev = NULL;
         return;
     }
-    int i=0;
+    int i = 0;
     node *temp = head;
-    while (i<pos-1){
+    while (i < pos - 1)
+    {
         temp = temp->next;
         i++;
     }
     ptr->next = temp->next;
     ptr->prev = temp;
     temp->next = ptr;
-
 }
-//INSERTING AFTER A PARTICULAR ELEMENT (PART5)
-void InsertAfterElement(int ele,int val){
-    node* ptr = (node*)malloc(sizeof(node));
+// INSERTING AFTER A PARTICULAR ELEMENT (PART5)
+void InsertAfterElement(int ele, int val)
+{
+    node *ptr = (node *)malloc(sizeof(node));
     ptr->data = val;
-    if(head == NULL){
+    if (head == NULL)
+    {
         printf("NO Element present");
         head = ptr;
         ptr->next = NULL;
@@ -82,52 +95,60 @@ void InsertAfterElement(int ele,int val){
         return;
     }
     node *temp = head;
-    while(temp->data != ele){
+    while (temp->data != ele)
+    {
         temp = temp->next;
     }
     ptr->next = temp->next;
     temp->next = ptr;
     ptr->prev = temp;
 }
-//INSERTING BEFORE A PARTICULAR ELEMENT (PART6)
-void InsertBeforeElement(int ele,int val){
-    node *ptr = (node*)malloc(sizeof(node));
+// INSERTING BEFORE A PARTICULAR ELEMENT (PART6)
+void InsertBeforeElement(int ele, int val)
+{
+    node *ptr = (node *)malloc(sizeof(node));
     ptr->data = val;
-     if(head == NULL){
+    if (head == NULL)
+    {
         head = ptr;
         ptr->next = NULL;
         ptr->prev = NULL;
         return;
     }
     node *temp = head;
-    while(temp->next->data != ele){
+    while (temp->next->data != ele)
+    {
         temp = temp->next;
     }
     ptr->next = temp->next;
     temp->next = ptr;
     ptr->prev = temp;
 }
-//DELETION AT THE END (PART2)
-void deleteAtEnd(){
+// DELETION AT THE END (PART2)
+void deleteAtEnd()
+{
     node *temp = head;
     node *ptr = head->next;
-     if(head == NULL){
-       free(head);
+    if (head == NULL)
+    {
+        free(head);
     }
-    while(ptr->next != NULL){
+    while (ptr->next != NULL)
+    {
         ptr = ptr->next;
         temp = temp->next;
     }
     free(ptr);
     temp->next = NULL;
-
 }
-//DELETION AT A SPECIFIC POSITION (PART4)
-void deleteAtPos(int pos){
+// DELETION AT A SPECIFIC POSITION (PART4)
+void deleteAtPos(int pos)
+{
     node *ptr = head;
     node *temp = head->next;
-    int i=0;
-    while(i!=pos-1){
+    int i = 0;
+    while (i != pos - 1)
+    {
         ptr = ptr->next;
         temp = temp->next;
         i++;
@@ -135,28 +156,32 @@ void deleteAtPos(int pos){
     ptr->next = temp->next;
     temp->next->prev = ptr;
     free(temp);
-
 }
 
-node revlist(){
-    node *previousNode = NULL;
-    node *current = head;
-    while(current != NULL){
-        previousNode = current->prev;
-      current->prev = current->next;
-      current->next = previousNode;
-      current = current->prev;
+void revlist()
+{
+    node *ptr1 = head;
+    node *ptr2 = ptr1->next;
 
+    ptr1->next = NULL;
+    ptr1->prev = ptr2;
+    while (ptr2 != NULL)
+    {
+        ptr2->prev = ptr2->next;
+        ptr2->next = ptr1;
+        ptr1 = ptr2;
+        ptr2 = ptr2->prev;
     }
-    head = previousNode;
+    head = ptr1;
 }
 
-int main(){
+int main()
+{
     InsertAtBeginning(10);
     insertAtEnd(20);
-    InsertAtPos(1,25);
-    InsertAfterElement(25,30);
-    InsertBeforeElement(30,40);
+    InsertAtPos(1, 25);
+    InsertAfterElement(25, 30);
+    InsertBeforeElement(30, 40);
     deleteAtEnd();
     deleteAtPos(1);
     revlist();
