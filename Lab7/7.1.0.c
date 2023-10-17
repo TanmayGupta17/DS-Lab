212c2e200d2054573fb58f8108aabf9099a05d7f
RushilJalal <rushiljalal@gmail.com>
2023-10-17 12:19:41 +0530

    Add createList(), fix code formatting

--- Lab7/7.1.0.c
+++ Lab7/7.1.0.c
@@ -1,41 +1,82 @@
-//insert in the beginning
-//insert at the end
-//insert at the given position
-//delete in the beginning
-//delete from the end
+// insert in the beginning
+// insert at the end
+// insert at the given position
+// delete in the beginning
+// delete from the end
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct Node {
+typedef struct Node
+{
     int data;
-    struct Node* next;
-};
+    struct Node *next;
+} Node;
 
-struct Node* head = NULL;
+Node *head = NULL;
 
-void TraverLinkedList(){
-    struct Node *ptr = head;
-    while(ptr != NULL){
+void createList()
+{
+    if (head == NULL)
+    {
+        int n;
+        printf("Enter number of nodes: ");
+        scanf("%d", &n);
+        if (n != 0)
+        {
+            int data;
+            Node *newNode;
+            Node *current;
+            newNode = malloc(sizeof(Node));
+            head = newNode;
+            current = head;
+            printf("Enter start data: ");
+            scanf("%d", &data);
+            head->data = data;
+            for (int i = 2; i <= n; i++)
+            {
+                newNode = malloc(sizeof(Node));
+                current->next = newNode;
+                printf("Enter data: ");
+                scanf("%d", &data);
+                newNode->data = data;
+                current = current->next;
+            }
+            current->next = NULL;
+            printf("List is created\n");
+        }
+    }
+    else
+        printf("List already created\n");
+}
+
+void TraverseLinkedList()
+{
+    Node *ptr = head;
+    while (ptr != NULL)
+    {
         printf("%d\n", ptr->data);
         ptr = ptr->next;
     }
 }
 
-void insertAtFirst(int val){
-    struct Node *p = (struct Node*)malloc(sizeof(struct Node));
+void insertAtFirst(int val)
+{
+    Node *p = (Node *)malloc(sizeof(struct Node));
     p->data = val;
     p->next = head;
     head = p;
     return;
 }
 
-void insertAtIndex(int val,int index){
-    struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
-    struct Node *p = head;
+void insertAtIndex(int val, int index)
+{
+    Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    Node *p = head;
     ptr->data = val;
-    int i=0;
-    while(i != index-1){
+    int i = 0;
+    while (i != index - 1)
+    {
         p = p->next;
         i++;
     }
@@ -44,79 +85,110 @@ void insertAtIndex(int val,int index){
     return;
 }
 
-void inserAtEnd(int val){
-        struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
-        ptr->data = val;
-        struct Node *p = head;
-        while(p->next != NULL){
-            p = p->next;
-        }
-        ptr->next = NULL;
-        p->next = ptr;
-        return head;
+void inserAtEnd(int val)
+{
+    Node *ptr = (Node *)malloc(sizeof(Node));
+    ptr->data = val;
+    Node *p = head;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    ptr->next = NULL;
+    p->next = ptr;
 }
 
-void DeleteAtFirst(){
-    struct Node *p = head;
+void DeleteAtFirst()
+{
+    Node *p = head;
     head = head->next;
     free(p);
     return;
-
 }
 
-void DeleteAtEnd(){
-    struct Node *ptr = head;
-    struct Node *p = head->next;
-    while(p->next != NULL){
+void DeleteAtEnd()
+{
+    Node *ptr = head;
+    Node *p = head->next;
+    while (p->next != NULL)
+    {
         p = p->next;
         ptr = ptr->next;
     }
     ptr->next = NULL;
     free(p);
-
 }
 
-void DeleteElement(int val){
-    struct Node *ptr = head;
-    struct Node *p = head->next;
-    while (p->data != val){
+void DeleteElement(int val)
+{
+    Node *ptr = head;
+    Node *p = head->next;
+    while (p->data != val)
+    {
         ptr = ptr->next;
         p = p->next;
     }
     ptr->next = p->next;
     free(p);
-
 }
 
-void sorting(){
-    struct Node *p = head;
-    struct Node *q = head->next;
-    while(p->next != NULL){
-        if(p->data > q->data){
+void sorting()
+{
+    Node *p = head;
+    Node *q = head->next;
+    while (p->next != NULL)
+    {
+        if (p->data > q->data)
+        {
             int temp = p->data;
             p->data = q->data;
             q->data = temp;
         }
-        else if ((p->data < q->data) && q->next != NULL){
+        else if ((p->data < q->data) && q->next != NULL)
+        {
             q = q->next;
         }
-        else{
+        else
+        {
             p = p->next;
             q = p;
             q = q->next;
         }
     }
-
 }
 
-int main(){
+void deleteAlt()
+{
+    int m = 0;
+    Node *current = head->next;
+    Node *ptr = head;
+    while (current != NULL)
+    {
+        if (m == 0)
+        {
+            ptr->next = current->next;
+            Node *temp = current;
+            current = current->next;
+            free(temp);
+            m = -1;
+        }
+        else
+        {
+            m = 0;
+            ptr = current;
+            current = current->next;
+        }
+    }
+}
 
-    TraverLinkedList();
+int main()
+{
+
+    TraverseLinkedList();
     insertAtFirst(91);
-    insertAtIndex(20,1);
+    insertAtIndex(20, 1);
     inserAtEnd(17);
     sorting();
     DeleteElement(20);
-    TraverLinkedList();
-
+    TraverseLinkedList();
 }
